dcb3fab1a80177264cee10c705974de4eb779c58
y-iyama <y-iyama@unext.jp>
2019-06-10 18:06:33 +0900

    no message

--- pages/test.js
+++ pages/test.js
@@ -10,15 +10,35 @@ import Spec from '../components/service/Spec--animated';
 
 const test = () => (
   <>
-    <KeyvisualLayout>
+    <Block>0px</Block>
+    <Block>100px</Block>
+    <Block>200px</Block>
+    <Block>300px</Block>
+    <Block>400px</Block>
+    <Block>500px</Block>
+    <Block>600px</Block>
+    <Block>700px</Block>
+
+    {/* <KeyvisualLayout>
       <AnimatedKeyvisual />
     </KeyvisualLayout>
     <TrackVisibility
       offset={300}
       once>
       <Spec />
-    </TrackVisibility>
+    </TrackVisibility> */}
   </>
 );
+
+const Block = styled.div`
+  background-color: #faa0cc;
+  color: white;
+  font-size: 30px;
+  height: 100px;
+  width: 100%;
+  &:nth-of-type(2n) {
+    background-color: #e00e87;
+  }
+`;
 
 export default withLayout(test);
