4a1841298eb5eced2483be4d9975e7ae659a210a
mac0032 <ka-kawamoto@unext.jp>
2019-06-11 17:52:42 +0900

    faqをマップで繰り返しのやり方に書き換えた

--- components/service/Faq/index.js
+++ components/service/Faq/index.js
@@ -1,10 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Row, Col, UncontrolledCollapse } from 'reactstrap';
-import { FaqBody, Title, FaqWrap, FaqList, DefList, DefTerm, DefDes, Arrow } from './Layout';
+import {
+  Container, Row, Col, UncontrolledCollapse,
+} from 'reactstrap';
+import {
+  FaqBody,
+  Title,
+  FaqWrap,
+  FaqList,
+  DefList,
+  DefTerm,
+  DefDes,
+  Arrow,
+} from './Layout';
 
 const Faq = () => {
-  console.log('WIP');
+  console.log('！！FAQの！！WIP！！だよ！！');
+
+  const QA = [
+    {
+      no: '01',
+      term: 'U-NEXTとは？',
+      description:
+        '株式会社USENで、2007年に始めた動画配信サービスです。現在は映画/ ドラマ/ アニメなどの動画だけではなく、マンガ/ ラノベ/ 書籍/ 雑誌など幅広いコンテンツを配信中。ひとつのサービスで観るも読むも、お楽しみいただけます。',
+    },
+    {
+      no: '02',
+      term: '⾒放題作品 / レンタル作品の本数は？',
+      description:
+        '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明',
+    },
+    {
+      no: '03',
+      term: 'なぜ⾒放題作品とレンタル作品があるの？',
+      description:
+        '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明',
+    },
+    {
+      no: '04',
+      term: '無料トライアルは何が無料？',
+      description:
+        '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明',
+    },
+    {
+      no: '05',
+      term: '最⼤40％※ポイントバックとは？',
+      description:
+        '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明',
+    },
+    {
+      no: '06',
+      term: 'フルHD画質/ 4K画質とは？',
+      description:
+        '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明',
+    },
+    {
+      no: '07',
+      term: 'いつでも解約できますか？',
+      description:
+        '説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明',
+    },
+  ];
+
   return (
     <>
       <Container>
@@ -13,78 +70,19 @@ const Faq = () => {
             <FaqBody>
               <Title>よくある質問</Title>
               <FaqWrap>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler01">
-                      U-NEXTとは？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler01">
-                      <DefDes>
-                        株式会社USENで、2007年に始めた動画配信サービスです。現在は映画/ ドラマ/ アニメなどの動画だけではなく、マンガ/ ラノベ/ 書籍/ 雑誌など幅広いコンテンツを配信中。ひとつのサービスで観るも読むも、お楽しみいただけます。       
-                      </DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler02">
-                      ⾒放題作品/ レンタル作品の本数は？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler02">
-                      <DefDes>説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler03">
-                      なぜ⾒放題作品とレンタル作品があるの？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler03">
-                      <DefDes>説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler04">
-                      無料トライアルは何が無料？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler04">
-                      <DefDes>説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler05">
-                      最⼤40％※ポイントバックとは？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler05">
-                          <DefDes>説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler06">
-                      フルHD画質/ 4K画質とは？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler06">
-                      <DefDes>説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
-                <FaqList>
-                  <DefList>
-                    <DefTerm id="toggler07">
-                      いつでも解約できますか？<Arrow></Arrow>
-                    </DefTerm>
-                    <UncontrolledCollapse toggler="#toggler07">
-                      <DefDes>説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</DefDes>
-                    </UncontrolledCollapse>
-                  </DefList>
-                </FaqList>
+                {QA.map((qa) => (
+                  <FaqList>
+                    <DefList>
+                      <DefTerm id={`toggler${qa.no}`}>
+                        {qa.term}
+                        <Arrow></Arrow>
+                      </DefTerm>
+                      <UncontrolledCollapse toggler={`#toggler${qa.no}`}>
+                        <DefDes>{qa.description}</DefDes>
+                      </UncontrolledCollapse>
+                    </DefList>
+                  </FaqList>
+                ))}
               </FaqWrap>
             </FaqBody>
           </Col>
