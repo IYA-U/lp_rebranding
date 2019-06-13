3dfa88431322a29fc0a23b9d9903f1a587412d53
mac0032 <ka-kawamoto@unext.jp>
2019-06-13 14:49:37 +0900

    Specを部品に分割

---
+++ components/service/Spec/Sl.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+import device from '../../../styleConfigs/breakPoints';
+
+const Sl = (props) => {
+  console.log(props);
+
+  return (
+    <>
+      <SpecList>
+        <DefList>
+          <DefTerm className={`${props.data.underline}`}>
+            <p>{props.data.spec}</p>
+          </DefTerm>
+          <DefDes className={`${props.data.underline}`}>
+            <p>{props.data.detail}</p>
+          </DefDes>
+        </DefList>
+      </SpecList>
+    </>
+  );
+};
+
+const SpecList = styled.li`
+  padding: 3.2rem 3rem;
+  &:nth-child(odd) {
+    background-color: #f7f7f7;
+  }
+  &:nth-child(even) {
+    background-color: #fff;
+  }
+  @media ${device.TAB} {
+    padding: 3rem 6rem;
+  }
+`;
+
+const DefList = styled.dl`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1.4rem;
+  font-weight: 400;
+  justify-content: space-between;
+  line-height: 1.4rem;
+  margin: 0;
+  padding: 0;
+  & > .underline {
+    p {
+      border-bottom: 1px solid #505050;
+    }
+  }
+  @media ${device.TAB} {
+    font-size: 3rem;
+    line-height: 3rem;
+  }
+`;
+
+const DefTerm = styled.dt`
+  flex: 1;
+  font-size: inherit;
+  font-weight: inherit;
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  & > p {
+    display: inline-block;
+    font-size: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+  }
+  @media ${device.TAB} {
+  }
+`;
+
+const DefDes = styled.dd`
+  margin: 0;
+  padding: 0;
+  & > p {
+    display: inline-block;
+    font-size: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+    margin: 0 auto;
+    & > img {
+      display: inline-block;
+      height: auto;
+      margin: auto;
+      width: 14px;
+      @media ${device.TAB} {
+        width: 28px;
+      }
+    }
+  }
+  @media ${device.TAB} {
+    width: 210px;
+    text-align: center;
+  }
+`;
+
+export default Sl;
---
+++ components/service/Spec/constants.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    spec: '無料トライアル',
+    detail: '31日間',
+    underline: null,
+  },
+  {
+    spec: '無料トライアル後の月額料金',
+    detail: '1,990円（税抜）',
+    underline: null,
+  },
+  {
+    spec: '毎月もらえるU-NEXTポイント',
+    detail: '1,200円',
+    underline: null,
+  },
+  {
+    spec: '同時視聴アカウント数',
+    detail: '4',
+    underline: null,
+  },
+  {
+    spec: 'アカウントごとの機能制限設定',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: 'underline',
+  },
+  {
+    spec: 'ポイントバック（ポイント還元）',
+    detail: '最大40%',
+    underline: 'underline',
+  },
+  {
+    spec: 'フルHD画質 / 4K画質',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: 'underline',
+  },
+  {
+    spec: '高音質（DOLBY DIGITAL PLUS）',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: null,
+  },
+  {
+    spec: 'テレビ / スマホ / PCなどに対応',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: null,
+  },
+  {
+    spec: 'ダウンロード機能',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: null,
+  },
+  {
+    spec: '動画も書籍も楽しめる',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: null,
+  },
+  {
+    spec: '見放題作品',
+    detail: '90,000本以上',
+    underline: null,
+  },
+  {
+    spec: '雑誌読み放題',
+    detail: '70誌以上',
+    underline: null,
+  },
+  {
+    spec: 'プロが選ぶおすすめ特集',
+    detail: '5,000以上',
+    underline: null,
+  },
+  {
+    spec: 'いつでも簡単に解約できる',
+    detail: <img src="/static/img/check@2x.png" />,
+    underline: null,
+  },
+];
