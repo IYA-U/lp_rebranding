abbe44b7790c67992acab3db11e6b35e164de6e7
mac0032 <ka-kawamoto@unext.jp>
2019-07-18 16:03:04 +0900

    タイトルを変数化、スタイル微調整

--- components/tetris/Thumbnails/index.js
+++ components/tetris/Thumbnails/index.js
@@ -5,12 +5,15 @@ import Thumbnail from './Thumbnail';
 import device from '../../../styleConfigs/breakPoints';
 import SubPatternWrap from '../common/SubPatternWrap';
 
-const Thumbnails = ({ thumbnails }) => {
+const Thumbnails = ({ thumbnails, title }) => {
   const isOdd = thumbnails.length % 2 > 0;
 
   return (
     <Container>
       <Wrap>
+        <Row>
+          <Title dangerouslySetInnerHTML={{ __html: title }} />
+        </Row>
         <Row>
           {thumbnails.map((thumbnail, index) => (
             <Col
@@ -28,13 +31,28 @@ const Thumbnails = ({ thumbnails }) => {
 };
 
 const Wrap = styled.div`
-  padding: 0 20px;
+  background-color: #fff;
+  padding: 90px 10px 70px;
   @media ${device.TAB} {
-    padding: 0 10px;
+    padding: 100px 10px 80px;
   }
   @media ${device.PC} {
-    padding: 0;
+    padding: 100px 10px 80px;
     margin: 0 -10px;
+  }
+`;
+
+const Title = styled.h2`
+  color: ${({ theme }) => theme.colors.dark};
+  font-size: 2.8rem;
+  font-weight: bold;
+  letter-spacing: 0.2rem;
+  line-height: 1.4;
+  margin: 0 auto 10px auto;
+  text-align: center;
+  @media ${device.PC} {
+    font-size: 4.4rem;
+    margin: 0 auto 20px auto;
   }
 `;
 
