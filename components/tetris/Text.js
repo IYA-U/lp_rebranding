4114f001883b882bfe3ebac847489b9f0c479939
m-sato <m-sato@unext.jp>
2019-07-18 19:43:30 +0900

    UXDWF-3514 text

--- components/tetris/Text.js
+++ components/tetris/Text.js
@@ -5,18 +5,10 @@ import device from '../../styleConfigs/breakPoints';
 import SubPatternWrap from './common/SubPatternWrap';
 
 const Text = ({ hdline, paragraph }) => (
-  <Container>
-    <Row>
-      <Col xs="12">
-        <Wrap>
-          {hdline && <Hdline dangerouslySetInnerHTML={{ __html: hdline }} />}
-          {paragraph && (
-            <Paragraph dangerouslySetInnerHTML={{ __html: paragraph }} />
-          )}
-        </Wrap>
-      </Col>
-    </Row>
-  </Container>
+  <Wrap>
+    {hdline && <Hdline dangerouslySetInnerHTML={{ __html: hdline }} />}
+    {paragraph && <Paragraph dangerouslySetInnerHTML={{ __html: paragraph }} />}
+  </Wrap>
 );
 
 const Hdline = styled.h2`
@@ -28,8 +20,11 @@ const Hdline = styled.h2`
   text-align: left;
 
   @media ${device.TAB} {
-    font-size: 44px;
+    font-size: 32px;
     text-align: center;
+  }
+  @media ${device.PC} {
+    font-size: 44px;
   }
 `;
 
@@ -38,6 +33,10 @@ const Paragraph = styled.p`
   font-size: 16px;
   margin: 40px auto 0 auto;
   text-align: justify;
+  @media ${device.TAB} {
+    margin: 40px auto 0 auto;
+    max-width: 640px;
+  }
   @media ${device.PC} {
     margin: 80px auto 0 auto;
     max-width: 640px;
@@ -45,28 +44,13 @@ const Paragraph = styled.p`
 `;
 
 const Wrap = styled.div`
-  background-color: rgba(${({ theme }) => theme.colors.black}, 0.8);
-  margin: 45px 0 80px;
-  padding: 0 20px;
-  @media ${device.TAB} {
-    margin: 45px 0 80px;
-  }
+  background-color: ${({ theme }) => theme.colors.black};
+  opacity: 0.7;
+  padding: 45px 20px 80px;
+
   @media ${device.PC} {
-    margin: 300px 0 300px;
+    padding: 300px 0 300px;
   }
 `;
 
-const BrPc = styled.br`
-  display: none;
-  @media ${device.TAB} {
-    display: block;
-  }
-`;
-
-const BrSp = styled.br`
-  display: block;
-  @media ${device.TAB} {
-    display: none;
-  }
-`;
 export default Text;
