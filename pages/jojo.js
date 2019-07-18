2e2e239b99389fb4e69529b2900a849b780df45e
mac0032 <ka-kawamoto@unext.jp>
2019-07-18 20:06:03 +0900

    jojoページを作成

--- pages/jojo.js
+++ pages/jojo.js
@@ -3,8 +3,13 @@ import withLayout from '../components/Layout/withLayout';
 import Thumbnails from '../components/tetris/Thumbnails';
 import Trailer from '../components/tetris/Trailer';
 import Cards from '../components/tetris/Cards';
-import Picture from '../components/tetris/Picture';
+import PictureFullWidth from '../components/tetris/PictureFullWidth';
 import Text from '../components/tetris/Text';
+import Book from '../components/tetris/Book';
+import Spec from '../components/service/Spec';
+import Trial from '../components/service/Trial';
+import Faq from '../components/service/Faq';
+import Feature from '../components/service/Feature';
 
 const Jojo = () => (
   <div>
@@ -25,55 +30,55 @@ const Jojo = () => (
           'https://streamh01.nxtv.jp/hls03/special/unext_promotion/twd_s9_2_trailer/mp4/twd_s9_2_trailer.mp4',
         webm:
           'https://streamh01.nxtv.jp/hls03/special/unext_promotion/twd_s9_2_trailer/webm/twd_s9_2_trailer.webm',
-        poster: '/static/img/twdVideoThumb.jpg',
+        poster: '/static/img/jojo/trailerfuta@2x.jpg',
       }}
     />
     <Cards
       cards={[
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara01@2x.jpg',
           title: 'ジョルノ・<br />ジョバァーナ',
           paragraph:
             '父親はジョースター家の宿敵DIO自分を救ってくれたギャングに憧れて、街を浄化するために、自身もギャングスターになる決意を抱いてギャング組織「パッショーネ」に入団。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara02@2x.jpg',
           title: 'ブローノ・<br />ブチャラティ',
           paragraph:
             'ギャング組織「パッショーネ」のチームリーダーの一人で、ポルポの部下。20歳。おかっぱのように切り揃えた髪に、白地に点が入り多数のジッパーが付いたスーツを着用している。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara03@2x.jpg',
           title: 'トリッシュ・<br />ウナ',
           paragraph:
             'パッショーネのボスの娘。1986年4月19日生まれ。15歳。四則演算の記号がプリントされたパレオのような服を着ている。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara04@2x.jpg',
           title: 'グイード・<br />ミスタ',
           paragraph:
             'ブチャラティの部下でチームのムードメーカー。18歳。ファッションは網模様の服と縞のズボン、網と丸点模様の帽子。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara05@2x.jpg',
           title: 'ナランチャ・<br/>ギルガ',
           paragraph:
             'ブチャラティの部下。17歳。小柄な少年で、布をカチューシャのように使って髪型を簡易に作っている。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara06@2x.jpg',
           title: 'レオーネ・<br />アバッキオ',
           paragraph:
             'ブチャラティの部下。21歳。銀色の長髪で唇に薄紫色のルージュを引いた男。元は正義感溢れる警察官だったが、腐敗した社会に失望、自らも収賄などの悪事に手を染めるようになる。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara07@2x.jpg',
           title: 'パンナコッタ・<br />フーゴ',
           paragraph:
             'ブチャラティの部下でチームのブレーン。1985年生まれの16歳。無数の大穴の空いた服と、イチゴ柄のネクタイを着用している。',
         },
         {
-          img: '/static/img/legalhigh.jpg',
+          img: '/static/img/jojo/img_chara01@2x.jpg',
           title: 'パンナコッタ・<br />フーゴ',
           paragraph:
             'ブチャラティの部下でチームのブレーン。1985年生まれの16歳。無数の大穴の空いた服と、イチゴ柄のネクタイを着用している。',
@@ -115,11 +120,67 @@ const Jojo = () => (
         },
       ]}
     />
-    <Picture
-      pictures={[
-        'https://imgc.nxtv.jp/img/info/titspot/00019/SID0019592_fwxga.png?output-format=jpeg&output-quality=60',
-      ]}
+    <Book
+      hdline="動画だけじゃない、<br/>ジョジョ漫画全巻を読むなら、U-NEXT"
+      books={{
+        aspectRatio: 'sss',
+        books: [
+          {
+            img:
+              'https://metac.nxtv.jp/img/bookimg/pubridge/00001860/BT000018607300100101.jpg',
+            link:
+              'https://www.video.unext.jp/book_title/BSD0000026894/BID0000040335',
+            alt: 'ジョジョの奇妙な冒険 第1部',
+          },
+          {
+            img:
+              'https://metac.nxtv.jp/img/bookimg/pubridge/00001789/BT000017899400700701.jpg',
+            link:
+              'https://www.video.unext.jp/book_title/BSD0000026894/BID0000040335',
+            alt: 'ジョジョの奇妙な冒険 第2部',
+          },
+          {
+            img:
+              'https://metac.nxtv.jp/img/bookimg/pubridge/00001860/BT000018607501001001.jpg',
+            link:
+              'https://www.video.unext.jp/book_title/BSD0000026894/BID0000040335',
+            alt: 'ジョジョの奇妙な冒険 第3部',
+          },
+          {
+            img:
+              'https://metac.nxtv.jp/img/bookimg/pubridge/00002027/BT000020278601801801.jpg',
+            link:
+              'https://www.video.unext.jp/book_title/BSD0000026894/BID0000040335',
+            alt: 'ジョジョの奇妙な冒険 第4部',
+          },
+          {
+            img:
+              'https://metac.nxtv.jp/img/bookimg/pubridge/00001844/BT000018447001001001.jpg',
+            link:
+              'https://www.video.unext.jp/book_title/BSD0000026894/BID0000040335',
+            alt: 'ジョジョの奇妙な冒険 第5部',
+          },
+          {
+            img:
+              'https://metac.nxtv.jp/img/bookimg/pubridge/00002362/BT000023620401701701.jpg',
+            link:
+              'https://www.video.unext.jp/book_title/BSD0000026894/BID0000040335',
+            alt: 'ジョジョの奇妙な冒険 第6部',
+          },
+        ],
+      }}
     />
+    <PictureFullWidth
+      srcPc="/static/img/jojo/pc/pictures.jpg"
+      srcSp="/static/img/jojo/sp/pictures.jpg"
+      alt="ジョジョのなんとかの冒険"
+    />
+    <Feature />
+    <div>共通パーツデバイス画像を入れるところ</div>
+    <Spec />
+    <div>共通パーツピクチャーを入れるところ</div>
+    <Trial />
+    <Faq />
   </div>
 );
 
