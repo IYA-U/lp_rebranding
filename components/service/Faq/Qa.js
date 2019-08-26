ede6335fbf64b44d5d15cc4a3a3f40b1f5da12a4
m-sato <m-sato@unext.jp>
2019-08-26 16:48:59 +0900

    UXDWF-3698 アコーディオン

--- components/service/Faq/Qa.js
+++ components/service/Faq/Qa.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { UncontrolledCollapse } from 'reactstrap';
 import device from '../../../styleConfigs/breakPoints';
 
-const Qa = (props) => (
-  <FaqList>
-    <DefList>
-      <DefTerm id={`toggler${props.data.no}`}>
-        {props.data.term}
-        <Arrow></Arrow>
-      </DefTerm>
-      <UncontrolledCollapse toggler={`#toggler${props.data.no}`}>
-        <DefDes>{props.data.description}</DefDes>
-      </UncontrolledCollapse>
-    </DefList>
-  </FaqList>
-);
+const Qa = ({ data }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  };
+  return (
+    <FaqList>
+      <DefList>
+        <DefTerm
+          isOpen={isOpen}
+          onClick={toggleMenu}>
+          {data.term}
+          <Arrow
+            isOpen={isOpen}
+            onClick={toggleMenu} />
+        </DefTerm>
+        <DefDes
+          isOpen={isOpen}
+          onClick={toggleMenu}>
+          {data.description}
+        </DefDes>
+      </DefList>
+    </FaqList>
+  );
+};
+
+Qa.propTypes = {};
 
 const FaqList = styled.li`
   height: auto;
@@ -41,20 +56,18 @@ const DefTerm = styled.dt`
   padding: 3rem 3.2rem 2.8rem 3.2rem;
   position: relative;
   vertical-align: middle;
+  cursor:pointer;
   @media ${device.TAB} {
     font-size: 2rem;
     line-height: 3rem;
     padding: 3.8rem 6rem 3.4rem 6rem;
-  }
-  .open {
-    &:after {
-      transform: rotate(90deg) !important;
     }
   }
 `;
 
 const DefDes = styled.dd`
   background-color: #f2f2f2;
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
   font-size: 1.4rem;
   font-weight: 300;
   letter-spacing: 0.2rem;
@@ -62,6 +75,8 @@ const DefDes = styled.dd`
   margin-top: 0.2rem;
   padding: 3rem 3.2rem;
   text-align: justify;
+  transition-duration: 0.5s;
+  transition-timing-function: ease-out;
   @media ${device.TAB} {
     margin-top: 0.4rem;
     padding: 3.2rem 6rem;
@@ -79,6 +94,7 @@ const Arrow = styled.span`
   position: absolute;
   right: 3.2rem;
   top: 0;
+
   width: 15px;
   @media ${device.TAB} {
     width: 18px;
@@ -95,6 +111,8 @@ const Arrow = styled.span`
     position: absolute;
     right: 0;
     top: 0;
+    transform: rotate(${({ isOpen }) => (isOpen ? '90deg' : '180deg')});
+    transition-duration: 0.5s;
     width: 2px;
     @media ${device.TAB} {
       width: 2px;
