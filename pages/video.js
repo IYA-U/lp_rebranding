34b13e1dd9a94ef0b9e49df2efb2ad720b9d4068
y-iyama <y-iyama@unext.jp>
2019-09-27 17:03:19 +0900

    videoテストページ追加

--- pages/video.js
+++ pages/video.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Index = () => (
   <>
-    <h1>LP実験</h1>
-
-    <video width="100%" autoPlay muted controls>
+    <video width="100%" autoPlay muted id="video" playsInline>
       <source src="/static/video/video.mp4" />
       動画を再生するには、videoタグをサポートしたブラウザが必要です。
     </video>
