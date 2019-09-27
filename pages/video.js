9390d42c3b78aaafd2547d5533804f929735294a
y-iyama <y-iyama@unext.jp>
2019-09-27 15:27:37 +0900

    no message

--- pages/video.js
+++ pages/video.js
@@ -4,7 +4,7 @@ const Index = () => (
   <>
     <h1>LP実験</h1>
 
-    <video width="100%" autoPlay muted>
+    <video width="100%" autoPlay muted controls>
       <source src="/static/video/video.mp4" />
       動画を再生するには、videoタグをサポートしたブラウザが必要です。
     </video>
