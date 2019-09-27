194bc740973ed20f7aa08d0966456170e5901c0f
y-iyama <y-iyama@unext.jp>
2019-09-27 15:19:55 +0900

    no message

---
+++ pages/video.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Index = () => (
+  <>
+    <h1>LP実験</h1>
+
+    <video width="100%" autoPlay muted>
+      <source src="/static/video/video.mp4" />
+      動画を再生するには、videoタグをサポートしたブラウザが必要です。
+    </video>
+  </>
+);
+
+export default Index;
